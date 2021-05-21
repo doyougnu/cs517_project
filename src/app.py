68f66296fe65571ab6fd5ddc59a7f71bdd637a73
doyougnu <jmy6342@gmail.com>
2021-05-20 19:37:06 -0700

    cycle check back up and working

--- src/app.py
+++ src/app.py
@@ -13,7 +13,8 @@ transparency in python. Think of it as a Reader Monad carrying the cache and
 solver handle.
 """
 
-from pprint import pprint
+from pprint     import pprint
+from functools  import reduce
 import z3       as z
 import igraph   as ig
 
@@ -23,39 +24,65 @@ import gadgets as g
 import graphs  as gs
 import utils   as u
 
+def symbolize_graph(cache,s,graph):
+    """Given a variable cache, a solver object and an igraph graph. Translate every
+    edge to a symbolic variable in the solver and return a new cache mapping
+    names to symbolics
 
-def find_cycle(cache,s,graph):
-    """Find a cycle in a graph. s is a solver object, graph is an adjacency list
-    e.g. {0:[0,1,2,3,4,5], 1:[2,3] ...}. Given the graph iterate over the
-    graph, convert the key and values to symbolic terms and check for a cycle.
-    Returns the unsat core as a list of strings e.g. [1->2, 2->3, 3->4, 4->1]
     """
-    for source, sink in graph.get_edgelist():
-        ## convert to strings
-        s_source = str(source)
-        s_sink   = str(sink)
 
-        ## create symbolics
-        sym_from = u.make_sym(cache,source)
-        sym_to   = u.make_sym(cache,sink)
+    ## the cache will only be populated once because symbolic edges will be
+    ## relaxes in the solver not in the python layer
+    pprint(u.cache_nodes(cache))
 
-        ## lets go, this is just a fold over the dict yielding ()
-        g.cycle_check(s, sym_from, sym_to, u.make_name(s_source,s_sink))
+    if not u.cache_nodes(cache):
+        for source, sink in graph.get_edgelist():
+            ## convert to strings
+            s_source = str(source)
+            s_sink   = str(sink)
+            s_edge   = u.make_name(s_source,s_sink)
+
+            ## create symbolics
+            cache, sym_source = u.make_sym_node(cache,s_source)
+            cache, sym_sink   = u.make_sym_node(cache,s_sink)
+            cache, sym_edge   = u.make_sym_edge(cache,s_edge)
+
+            ## pack the symbolic adjacency list
+            cache = u.add_adjacency(cache,sym_source,sym_sink)
+
+
+    return cache
+
+
+
+def find_all_cycles(ch,s,graph):
+    """Find a cycle in a graph. s is a solver object, graph is an igraph object.
+    Given the graph iterate over the graph, convert the key and values to
+    symbolic terms and check for a cycle. Returns the unsat core as a list of
+    strings e.g. [1->2, 2->3, 3->4, 4->1]
+    """
+
+    ## get the symbolics
+    cache = symbolize_graph(ch,s,graph)
+
+    print(cache)
+
+    ## enter new assertion level
+    s.push()
+    ## we have to use all edges or else this is violated
+    # s.add(z.Product(list(u.cache_edges(cache).)) > 0)
+    ## We cannot have cycles
+    g.cycle_check(s,cache)
 
     r = []
-    if s.check() == z.unsat: r = s.unsat_core()
-    return u.parse_core(r)
+    print(s.check())
+    if s.check() == z.unsat:
+        r = s.unsat_core()
 
-def relax(graph, unsat_core, strategy):
-    """Take a graph, an unsat-core and a strategy. use the strategy to find the
-    edge to relax, relatx the edge and return the graph
+    s.pop()
 
-    This function mutates graph
-    """
-    source, sink = strategy(unsat_core)
-    u.remove_edge(graph, int(source),int(sink))
+    return r
 
-    return graph
 
 def runWithGraph(cache,s,graph):
     # flag to end loop
@@ -63,23 +90,23 @@ def runWithGraph(cache,s,graph):
 
     # kick off
     while not done:
-        # nx.draw(graph)
-        # plt.show()
 
         # get the core
-        core = find_cycle(cache, s, graph)
+        cores = find_all_cycles(cache, s, graph)
 
         # if the core is empty then we are done, if not then relax and recur
-        print("Core: ", core)
-        if core:
-            # relax an edge by some strategy
-            graph = relax(graph, core, lambda x : x[0])
+        print("Core: ", cores)
 
-            # this is good enough for now but in the future we should try to
-            # remove only the right constraint, which will be much much faster
-            s.reset()
-        else:
-            done = True
+        done = True
+        # if core:
+        #     # relax an edge by some strategy
+        #     graph = relax(graph, core, lambda x : x[0])
+
+        #     # this is good enough for now but in the future we should try to
+        #     # remove only the right constraint, which will be much much faster
+        #     s.reset()
+        # else:
+        #     done = True
 
         # print("going: ", done)
         # print("graph Nodes: ", graph.nodes())
@@ -88,12 +115,11 @@ def runWithGraph(cache,s,graph):
     return graph
 
 def run():
-    cache = {}
+    cache = u.new_cache()
 
     # spin up the solver
     s = z.Solver()
 
-    # g = ig.Graph.Erdos_Renyi(n=15,p=0.2)
     g = gs.round_robin_graph
     newG = runWithGraph(cache,s,g)
     print(newG.is_dag())
--- src/gadgets.py
+++ src/gadgets.py
@@ -10,13 +10,22 @@ Module which defines gadgets for minimum feedback arc set encoding to SAT
 """
 
 from z3 import *
+import utils as u
 
-def cycle_check(s,v_from,v_to,name):
+def cycle_check(s,cache):
     """The cycle check is extremely simple, number the vertices in the graph from 0
     to n, then for each edge from v_from to v_to create a constraint that
     v_from < v_to. This encoding simply counts the edges that are traversed
     (and is similar to deBruijn indices actually!) so a cycle is the occurrence
-    of a vertex from an Int i to an Int j where i < j.
+    of a vertex from an Int i to an Int j where i < j. We add an extra
+    constraint that the edge is not indicated by a 0. In the solver a 0 on an
+    edge means the edge has been relaxed in the symbolic graph
+
     """
-    # edge_constraint
-    s.assert_and_track(v_from < v_to, name)
+
+    for sym_source, sinks in u.cache_adj_list(cache).items():
+        for sym_sink in sinks:
+            # # edge_constraint
+            edge_name = u.make_name(str(sym_source), str(sym_sink))
+            s.assert_and_track(sym_source < sym_sink, edge_name)
+            s.add(u.cache_edges(cache)[edge_name] != 0)
