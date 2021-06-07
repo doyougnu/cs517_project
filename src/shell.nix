cec600cd3ed0a79c81706a7a5c13f519c41eff39
doyougnu <jmy6342@gmail.com>
2021-06-06 22:04:37 -0700

    fix python path in nix shell for pytest

--- src/shell.nix
+++ src/shell.nix
@@ -28,6 +28,7 @@ in mkShell {
     # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
     export PIP_PREFIX=$(pwd)/_build/pip_packages
     export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+    export PYTHONPATH="$(pwd):$PYTHONPATH"
     export PATH="$PIP_PREFIX/bin:$PATH"
     unset SOURCE_DATE_EPOCH
   '';
